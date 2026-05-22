b779ba2a14022d7aab212aaa50ef193614f1351b
Jaya Makasre <jayamakasre2004@gmail.com>
2026-05-22 18:38:47 +0530

    Prepared project for deployment

--- employee_project/settings.py
+++ employee_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dh(5hv032su%p3*+k#jwftqs!nvx_is8zg@0qiqf6)y3pho))q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
