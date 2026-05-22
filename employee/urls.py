9f8194577b670c3205606218af8b6e3be52abf69
Jaya Makasre <jayamakasre2004@gmail.com>
2026-05-22 20:18:18 +0530

    Fixed URLs

--- employee/urls.py
+++ employee/urls.py
@@ -2,10 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('' , views.employee_list, name='employee_list'),
+
+    path('', views.employee_list, name='employee_list'),
+
     path('add/', views.add_employee, name='add_employee'),
-    path('delete/<int:id>/', views.delete_employee, name='delete_employee'),
+
     path('update/<int:id>/', views.update_employee, name='update_employee'),
+
+    path('delete/<int:id>/', views.delete_employee, name='delete_employee'),
+
     path('login/', views.login_user, name='login'),
-    path('logout/', views.logout_user, name='logout'),
+
 ]
